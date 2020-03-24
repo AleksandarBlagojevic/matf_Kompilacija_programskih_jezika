7daa9170a5ba8902b1aa064ba6da82c411b997d8
Aca <aleksandar.blagojevic.matf@gmail.com>
2020-03-24 13:03:37 +0100

    Dodat je header file klase Translacija

---
+++ Vektori/c/transformacija.hpp
@@ -0,0 +1,40 @@
+#ifndef __TRANSFORMACIJA_HPP__
+#define __TRANSFORMACIJA_HPP__ 1
+
+#include "vektor.hpp"
+
+class Transformacija {
+public:
+    virtual Vektor prebaci(const Vektor& v) const = 0;
+    virtual Transformacija *kopija() const = 0;
+    virtual ~Transformacija() { }
+};
+
+class Rotacija : public Transformacija {
+public:
+    Rotacija(double ugao) : _ugao(ugao) { }
+    Vektor prebaci(const Vektor& v) const;
+    Rotacija *kopija() const;
+private:
+    double _ugao;
+};
+
+class Homotetija : public Transformacija {
+public:
+    Homotetija(double koef) : _koef(koef) { }
+    Vektor prebaci(const Vektor& v) const;
+    Homotetija *kopija() const;
+private:
+    double _koef;
+};
+
+class Translacija : public Transformacija {
+public:
+    Translacija(Vektor v) : _v(v) { }
+    Vektor prebaci(const Vektor& v) const;
+    Translacija *kopija() const;
+private:
+    Vektor _v;
+};
+
+#endif
