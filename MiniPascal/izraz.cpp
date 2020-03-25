b9ee8ee09af9b56028339bea18eb96dd1b90c23c
Aca <aleksandar.blagojevic.matf@gmail.com>
2020-03-25 13:57:12 +0100

    Dodata je klasa Izraz, koja predstavlja matematicke izraze

---
+++ MiniPascal/izraz.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include "izraz.hpp"
+#include <map>
+using namespace std;
+
+extern map<string, int> tablica;
+
+Izraz::~Izraz() {
+  
+}
+
+int Konstanta::vrednost() const {
+  return _vrednost;
+}
+
+Izraz* Konstanta::kopija() const {
+  return new Konstanta(*this);
+}
+
+int Promenljiva::vrednost() const {
+  return tablica[_ime];
+}
+
+Izraz* Promenljiva::kopija() const {
+  return new Promenljiva(*this);
+}
+
+BinOp::~BinOp() {
+  delete _levi;
+  delete _desni;
+}
+
+BinOp::BinOp(const BinOp& z) {
+  _levi = z._levi->kopija();
+  _desni = z._desni->kopija();
+}
+
+BinOp& BinOp::operator=(const BinOp& z) {
+  if (this != &z) {
+    delete _levi;
+    delete _desni;
+    _levi = z._levi->kopija();
+    _desni = z._desni->kopija();
+  }
+  return *this;
+}
+
+int Zbir::vrednost() const {
+  return _levi->vrednost() + _desni->vrednost();
+}
+
+Izraz* Zbir::kopija() const {
+  return new Zbir(*this);
+}
+
+int Razlika::vrednost() const {
+  return _levi->vrednost() - _desni->vrednost();
+}
+
+Izraz* Razlika::kopija() const {
+  return new Razlika(*this);
+}
+
+int Proizvod::vrednost() const {
+  return _levi->vrednost() * _desni->vrednost();
+}
+
+Izraz* Proizvod::kopija() const {
+  return new Proizvod(*this);
+}
+
+int Kolicnik::vrednost() const {
+  return _levi->vrednost() / _desni->vrednost();
+}
+
+Izraz* Kolicnik::kopija() const {
+  return new Kolicnik(*this);
+}
+
+int Mod::vrednost() const {
+  return _levi->vrednost() % _desni->vrednost();
+}
+
+Izraz* Mod::kopija() const {
+  return new Mod(*this);
+}
+
+int Manje::vrednost() const {
+  return _levi->vrednost() < _desni->vrednost();
+}
+
+Izraz* Manje::kopija() const {
+  return new Manje(*this);
+}
+
+int Vece::vrednost() const {
+  return _levi->vrednost() > _desni->vrednost();
+}
+
+Izraz* Vece::kopija() const {
+  return new Vece(*this);
+}
+
+/*
+int main() {
+
+  tablica["x"] = 1;
+  tablica["y"] = 2;
+  
+  Izraz *i1 = new Konstanta(3);
+  
+  cout << i1->vrednost() << endl;
+
+  Izraz *i2 = new Promenljiva("x");
+
+  cout << i2->vrednost() << endl;
+
+  Izraz *i3 = new Zbir(i1, i2);
+  cout << i3->vrednost() << endl;
+  
+  delete i1;
+  delete i2;
+  delete i3;
+  
+  return 0;
+}
+*/
